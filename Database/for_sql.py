4450b370cbb144217891ae417fcfe5f1c551e3ac
HungNguyenDang <nghungi123@gmail.com>
2025-01-01 23:27:19 +0700

    manual - auto

--- Database/for_sql.py
+++ Database/for_sql.py
@@ -35,41 +35,72 @@ username = 'trading'
 password = 'Dulieulon123'
 driver = '{ODBC Driver 17 for SQL Server}'
 driver_alchemy = 'ODBC+Driver+17+for+SQL+Server'
-db_name = "audusd"
+prefix = "audusd" # prefix for database name, table name
+db_name = prefix
 table_name = "audusd_h6"
 pair_name = "AUDUSD.sml"
 timeframe = mt5.TIMEFRAME_H6
 unique_text = 'text_id'
 from_day = pd.to_datetime("2000-01-01 00:00:00")
 to_day = pd.to_datetime("2025-01-01 00:00:00")
-chunk_capacity = 20000
+chunk_capacity = 20000 # size of each time to insert data to sql
 # endregion
 
-timeframe_list = {mt5.TIMEFRAME_M2, mt5.TIMEFRAME_M3,
-             mt5.TIMEFRAME_M10,
-             mt5.TIMEFRAME_M30, mt5.TIMEFRAME_H1, mt5.TIMEFRAME_H2,
-             mt5.TIMEFRAME_H3, mt5.TIMEFRAME_H4, 
-             mt5.TIMEFRAME_W1, mt5.TIMEFRAME_MN1}
-table_name_list = {"audusd_m2", "audusd_m3",
-             "audusd_m10",
-             "audusd_m30", "audusd_h1", "audusd_h2",
-             "audusd_h3", "audusd_h4", 
-             "audusd_w1", "audusd_mn1"}
+timeframe_list = [mt5.TIMEFRAME_M1, mt5.TIMEFRAME_M2, mt5.TIMEFRAME_M3, mt5.TIMEFRAME_M4,
+                  mt5.TIMEFRAME_M5, mt5.TIMEFRAME_M10, mt5.TIMEFRAME_M15,
+                  mt5.TIMEFRAME_M30, mt5.TIMEFRAME_H1, mt5.TIMEFRAME_H2,
+                  mt5.TIMEFRAME_H3, mt5.TIMEFRAME_H4, mt5.TIMEFRAME_H6, mt5.TIMEFRAME_D1, 
+                  mt5.TIMEFRAME_W1, mt5.TIMEFRAME_MN1]
 
-# for tf, tbn in zip(timeframe_list, table_name_list): 
-#     print(f"Timeframe: {tf}, Table Name: {tbn}")      
+table_name_list = [f"{prefix}_m1", f"{prefix}_m2", f"{prefix}_m3", f"{prefix}_m4",
+                   f"{prefix}_m5", f"{prefix}_m10", f"{prefix}_m15",
+                   f"{prefix}_m30", f"{prefix}_h1", f"{prefix}_h2",
+                   f"{prefix}_h3", f"{prefix}_h4", f"{prefix}_h6", f"{prefix}_d1", 
+                   f"{prefix}_w1", f"{prefix}_mn1"]
 
 Server = Create_Server(driver, server, username, password, db_name)
 create_table(Server, table_name)
 
-# take data from OANDA MT5, split into smaller chunks for processing
+# The account for mt5
 account = Account(6269482, 'jc6qhduy', 'OANDA-Demo-1')
-pair = Pair(pair_name, timeframe)
-date_ranges = generate_date_ranges(2000, 2025)
-ohlc_df = get_data_MT5(link, account, pair, from_day, to_day)
-print("number of rows from mt5: ", len(ohlc_df))
-df_chunk = divide_dataframe(ohlc_df, chunk_capacity)
 
-# add data to the table
+# The engine for sql database
 engine = create_engine(f"mssql+pyodbc://{Server.username}:{Server.password}@{Server.server}/{Server.db_name}?driver={driver_alchemy}")
-add_data(table_name, engine, df_chunk)
+
+# --------------------------------------------------------------------------------------#
+manual = 1
+# -- Manual side, take the pair name, database name, table name, timeframe and run it
+if manual == 1:
+    # date_ranges = generate_date_ranges(2000, 2025)
+    pair = Pair(pair_name, timeframe)
+    ohlc_df = get_data_MT5(link, account, pair, from_day, to_day)
+    print("number of rows from mt5: ", len(ohlc_df))
+    df_chunk = divide_dataframe(ohlc_df, chunk_capacity)
+
+    # add data to the table
+
+    add_data(table_name, engine, df_chunk)
+
+# --------------------------------------------------------------------------------------#
+automatic = 0
+# -- Automatic side, process all timeframe for 1 pair
+if automatic == 1:
+    # Create a dictionary with the two lists
+    data = {
+        "Timeframe": timeframe_list,
+        "Table Name": table_name_list
+    }
+
+    # Create DataFrame
+    df_id = pd.DataFrame(data)
+
+    for index, row in df_id.iterrows():
+        # print(row['Timeframe'], ", ", row['Table Name'])
+        pair = Pair(pair_name, row['Timeframe'])
+        ohlc_df = get_data_MT5(link, account, pair, from_day, to_day)
+        print("number of rows from mt5: ", len(ohlc_df))
+        df_chunk = divide_dataframe(ohlc_df, chunk_capacity)
+
+        # add data to the table
+        add_data(row['Table Name'], engine, df_chunk)
+        
